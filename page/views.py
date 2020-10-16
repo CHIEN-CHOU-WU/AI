a7b7a1a95d5a90840648a565b05ed8bf2352d4bd
Chien-Chou Wu <johnson12440@gmail.com>
2020-10-16 18:22:44 -0500

    Predict of students' grade works

--- page/views.py
+++ page/views.py
@@ -1,13 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from .models import studentperformance
 # Create your views here.
 def home_view(request, *args, **kwargs):
 	#context = {'object': studentperformance.objects.all()}
 	#return HttpResponse("<h1>Django 版本!<h1>")
-	obj = studentperformance.objects.all()
-	context = {
-		'object': obj
-	}
-	return render(request, "page/home.html", context)
+
+	return render(request, "page/home.html", {})
